33f4b40cf4f734fbaabdb92a952631c70789c9a6
milokc <alanmilok@gmail.com>
2026-08-10 23:07:23 +0300

    Fixed file naming issue

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 def copy_file(command: str) -> None:
-    splited = command.split()
-    if len(splited) == 3 and splited[1] != splited[2] and splited[0] == "cp":
-        file_old = splited[1]
-        file_new = splited[2]
+    spl_cmd = command.split()
+    if len(spl_cmd) == 3 and spl_cmd[1] != spl_cmd[2] and spl_cmd[0] == "cp":
+        file_old = spl_cmd[1]
+        file_new = spl_cmd[2]
         try:
             with (open(file_old, "r") as file_in,
                   open(file_new, "w") as file_out):
